cde6ba5a7f692de4bf9b95902fa6e707029b4813
liaoqixing <hz19044055@huize.com>
2020-04-19 20:04:20 +0800

    feat: 107二叉树的层次遍历

---
+++ src/com/liao/leetcode/Solution107.java
@@ -0,0 +1,67 @@
+package com.liao.leetcode;
+
+//给定一个二叉树，返回其节点值自底向上的层次遍历。 （即按从叶子节点所在层到根节点所在的层，逐层从左向右遍历）
+//
+// 例如：
+//给定二叉树 [3,9,20,null,null,15,7],
+//
+//     3
+//   / \
+//  9  20
+//    /  \
+//   15   7
+//
+//
+// 返回其自底向上的层次遍历为：
+//
+// [
+//  [15,7],
+//  [9,20],
+//  [3]
+//]
+//
+// Related Topics 树 广度优先搜索
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution107 {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        LinkedList<List<Integer>> res = new LinkedList<>();
+        if (root == null) {
+            return res;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            List<Integer> one = new LinkedList<>();
+            int count = queue.size();
+            for (int i = 0; i < count; i++) {
+                TreeNode node = queue.poll();
+                one.add(node.val);
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+            }
+            res.addFirst(one);
+        }
+        return res;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
