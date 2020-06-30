940a46bea2e256e42f4d63ae18256f881b750f47
holyliao <holy468@live.com>
2020-06-30 22:56:49 +0800

    feat:387 字符串中的第一个唯一字符

---
+++ src/com/liao/leetcode/Solution387.java
@@ -0,0 +1,41 @@
+package com.liao.leetcode;
+
+//给定一个字符串，找到它的第一个不重复的字符，并返回它的索引。如果不存在，则返回 -1。
+//
+//
+//
+// 示例：
+//
+// s = "leetcode"
+//返回 0
+//
+//s = "loveleetcode"
+//返回 2
+//
+//
+//
+//
+// 提示：你可以假定该字符串只包含小写字母。
+// Related Topics 哈希表 字符串
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+
+    public int firstUniqChar(String s) {
+        int[] caps = new int[26];
+        char[] chars = s.toCharArray();
+        for (char c : chars) {
+            caps[c - 'a'] = caps[c - 'a'] + 1;
+        }
+
+        for (int i = 0; i < chars.length; i++) {
+            char c = chars[i];
+            if (caps[c - 'a'] == 1) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
