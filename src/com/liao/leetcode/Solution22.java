53c49eb6760a34bf156f01eb0e8945cd0611781f
holyliao <holy468@live.com>
2022-06-05 22:32:53 +0800

    add[22]:括号生成

--- src/com/liao/leetcode/Solution22.java
+++ src/com/liao/leetcode/Solution22.java
@@ -3,6 +3,30 @@ package com.liao.leetcode;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * 22. 括号生成
+ * 数字 n 代表生成括号的对数，请你设计一个函数，用于能够生成所有可能的并且 有效的 括号组合。
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：n = 3
+ * 输出：["((()))","(()())","(())()","()(())","()()()"]
+ * 示例 2：
+ * <p>
+ * 输入：n = 1
+ * 输出：["()"]
+ *  
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= n <= 8
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode.cn/problems/generate-parentheses
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
 public class Solution22 {
     public List<String> generateParenthesis(int n) {
         List<String> ans = new ArrayList<>();
