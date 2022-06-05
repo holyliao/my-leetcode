1cbd83bc0962b5e8e963ae75da02140c19425ddb
holyliao <holy468@live.com>
2022-06-05 22:27:45 +0800

    add[22]:括号生成

---
+++ src/com/liao/leetcode/Solution22.java
@@ -0,0 +1,38 @@
+package com.liao.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution22 {
+    public List<String> generateParenthesis(int n) {
+        List<String> ans = new ArrayList<>();
+
+        backtrack(ans, new StringBuilder(), 0, 0, n);
+        return ans;
+    }
+
+    private void backtrack(List<String> ans, StringBuilder cur, int open, int close, int max) {
+        if (cur.length() == max * 2) {
+            ans.add(cur.toString());
+            return;
+        }
+
+        if (open < max) {
+            cur.append('(');
+            backtrack(ans, cur, open + 1, close, max);
+            cur.deleteCharAt(cur.length() - 1);
+        }
+
+        if (close < open) {
+            cur.append(')');
+            backtrack(ans, cur, open, close + 1, max);
+            cur.deleteCharAt(cur.length() - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution22 s = new Solution22();
+        s.generateParenthesis(3);
+    }
+
+}
